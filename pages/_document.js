fddeb0b1665bdc3a03c36131c733c3accb48ce87
usmanwarind46 <usmanwarind.vmi@gmail.com>
2026-06-02 18:04:10 +0500

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,22 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/vercel.ico" />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-GB2G3KMRVD"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-GB2G3KMRVD');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
