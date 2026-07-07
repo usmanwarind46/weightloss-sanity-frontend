bb0ab7050a5704c6d6f3f22b31a93ee0617c807e
usmanwarind46 <usmanwarind.vmi@gmail.com>
2026-07-07 12:22:01 +0500

    update

--- pages/weight-loss-treatments/wegovy-pill.js
+++ pages/weight-loss-treatments/wegovy-pill.js
@@ -638,12 +638,12 @@ export default function MounjaroProduct({
               <WaitlistForm />
             </div> */}
 
-            <p className="text-sm text-amber-700 bg-amber-50 border border-amber-200 rounded-lg px-4 py-3 para-font mt-2">
+            {/* <p className="text-sm text-amber-700 bg-amber-50 border border-amber-200 rounded-lg px-4 py-3 para-font mt-2">
               <strong className="semibold-font">Please note:</strong> This is a
               pre-order to reserve your stock. Deliveries will commence from{" "}
               <strong className="semibold-font">7 July 2026</strong> onwards.
               Taking pre-orders for 1.5 mg and 4 mg strengths only.
-            </p>
+            </p> */}
 
             {/* CONSULTATION */}
             <div className="mt-2 bg-gray-100  rounded-xl p-5">
