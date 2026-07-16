9daa1512d3002bd87c442d17ab2a194794fa5762
usmanwarind46 <usmanwarind.vmi@gmail.com>
2026-07-16 18:13:59 +0500

    changes

--- components/HeroTwo.js
+++ components/HeroTwo.js
@@ -9,7 +9,7 @@ const slides = [
   {
     id: 1,
     type: "woman",
-    src: "/images/hero-three.png",
+    src: "/Images/hero-three.png",
     alt: "Weight loss treatment product with model",
   },
   {
@@ -24,13 +24,13 @@ export function HeroTwo({ ctaLink = "/weight-loss-treatments" }) {
 
   const activeSlide = slides[current];
 
-  // useEffect(() => {
-  //   const timer = window.setInterval(() => {
-  //     setCurrent((previous) => (previous + 1) % slides.length);
-  //   }, 4500);
+  useEffect(() => {
+    const timer = window.setInterval(() => {
+      setCurrent((previous) => (previous + 1) % slides.length);
+    }, 4500);
 
-  //   return () => window.clearInterval(timer);
-  // }, []);
+    return () => window.clearInterval(timer);
+  }, []);
 
   const handleTreatmentClick = () => {
     window.location.href = ctaLink;
@@ -41,7 +41,7 @@ export function HeroTwo({ ctaLink = "/weight-loss-treatments" }) {
       {/* Static background */}
       <div className="pointer-events-none absolute inset-0 -z-20">
         <Image
-          src="/images/hero-background.jpg"
+          src="/Images/hero-background.jpg"
           fill
           priority
           sizes="100vw"
@@ -389,7 +389,7 @@ function GraphSlide() {
           "
         >
           <Image
-            src="/images/hero-badge.png"
+            src="/Images/hero-badge.png"
             fill
             priority
             sizes="58vw"
@@ -422,7 +422,7 @@ function GraphSlide() {
           "
         >
           <Image
-            src="/images/hero-graph.png"
+            src="/Images/hero-graph.png"
             width={800}
             height={500}
             priority
@@ -493,7 +493,7 @@ function GraphSlide() {
           "
         >
           <Image
-            src="/images/hero-badge.png"
+            src="/Images/hero-badge.png"
             fill
             priority
             sizes="32vw"
@@ -525,7 +525,7 @@ function GraphSlide() {
           "
         >
           <Image
-            src="/images/hero-graph.png"
+            src="/Images/hero-graph.png"
             width={800}
             height={500}
             priority
